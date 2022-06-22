68b4d9086a94a3ff0febce18b1575fd3991d83b5
enduri <enduri@randomforest.eu>
2022-06-22 10:37:17 +0200

    test: initial commit

---
+++ cypress/steps/common/global.ts
@@ -0,0 +1,5 @@
+import { Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Then("Common step", () => {
+  cy.log("Common step works");
+});
---
+++ features/feature1/feature1A/xyz.feature
@@ -0,0 +1,5 @@
+Feature: Test feature file
+
+    Scenario: Test scenario
+        Given Common step
+        Then Feature step
